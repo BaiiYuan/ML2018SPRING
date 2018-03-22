8bed105ae5fe1c4a5b47c7bd4709b09eb61a1a22
BaiiYuan <b05902002@ntu.edu.tw>
2018-03-22 10:45:06 +0800

    delete

--- hw1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# python3 hw1.py "../HW1/test.csv" "sub-hw1.csv"
-python3 hw1.py $1 $2
--- hw1_best.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# python3 hw1_best.py "./HW1/test.csv" "./sub-hw1_best.csv"
-python3 hw1_best.py $1 $2
