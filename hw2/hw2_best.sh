7c405860650b682912be9c53eefc5d880c6728e1
BaiiYuan <b05902002@ntu.edu.tw>
2018-04-04 20:07:44 +0800

    revise best

--- hw2/hw2_best.sh
+++ hw2/hw2_best.sh
@@ -1,7 +1,7 @@
 # bash ./hw2_best.sh $1 $2 $3 $4 $5 $6
 echo start training
 # python3 hw2_best_train.py $3 $4
-# python3 hw2_best_train.py ../../hw2/train_X ../../hw2/train_Y
+python3 hw2_best_train.py ../../hw2/train_X ../../hw2/train_Y
 echo end training
 echo start testing
 # python3 hw2_best_test.py $3 $4 $5 $6
